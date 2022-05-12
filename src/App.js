4b4a83ff380ff67e4c3f4666b119c48811d8aff3
devshakti78 <shakti.dev46@gmail.com>
2022-05-12 13:50:20 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import  Pricing  from './Components/Home';
 import Album from './Components/Product';
 import { Footer } from './Components/Footer';
 import ButtonBases from './Components/Products2'
+import  Home  from './Components/Home';
 function App() {
   return (
     <div>
 <Pricing/>
-<Album/>
-<ButtonBases/>
-<Footer/>
 
+<ButtonBases/>
+
+<BrowserRouter>
+      <Routes>
+        
+          <Route path="product" element={<Album />} />
+       
+      </Routes>
+    </BrowserRouter>
+    <Footer/>
     </div>
   );
 }
