04a8f513f202767f40f86396971c6c8b03ef1f0b
devshakti78 <shakti.dev46@gmail.com>
2022-05-14 17:54:49 +0530

    search bar integrated

--- src/Components/Cartitems.jsx
+++ src/Components/Cartitems.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import Itemcard from './Itemcard'
 import data from './data'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const Cartitems = () => {
   return (
     <>
 <h1 className='text-center mt-3'>All items</h1>
+<div className='justify-content-center'>
+<FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small">Sort by price</InputLabel>
+      <Select
+        labelId="demo-select-small"
+        id="demo-select-small"
+        label="Sort by price"
+       
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        <MenuItem value={10}>Low To High</MenuItem>
+        <MenuItem value={20}>High To Low</MenuItem>
+        <MenuItem value={30}>All item</MenuItem>
+      </Select>
+    </FormControl>
+</div>
 <section className='py-4 container'>
 <div className='row justify-content-center'>
 {data.productData.map((item,index)=>{
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,6 +21,52 @@ import AlarmIcon from '@mui/icons-material/Alarm';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import Avatar from '@mui/material/Avatar';
 
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
@@ -76,7 +122,17 @@ function PricingContent() {
           <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
             Younger Choice ltd
           </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search… "
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
           <nav>
+         
             <Link
               variant="button"
               color="text.primary"
