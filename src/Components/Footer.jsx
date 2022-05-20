a2b379bfcee0fb8ac6d67cdbf8e84a35d7176c6c
devshakti78 <shakti.dev46@gmail.com>
2022-05-20 16:31:54 +0530

    done final

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -45,7 +45,7 @@ function Copyright() {
       <Typography variant="body2" color="text.secondary" align="center">
         {'Copyright © '}
         <Link color="inherit" href="https://mui.com/">
-          Your Website
+         YoungerChoiceLtd
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
